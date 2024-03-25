d41f69b7ee1b87c634d0ef22b1e358b48c7f7add
nizamuddin0172 <neom01723441081@gmail.com>
2024-03-26 01:59:27 +0600

    add show all button

--- script/index.js
+++ script/index.js
@@ -1,23 +1,36 @@
 
+const loadPhone = async (searchText, isShowAll) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+  );
+  const data = await res.json();
+  const phones = data.data;
+  // console.log(phones);
+  displayPhones(phones, isShowAll);
+};
 
-const loadPhone = async (searchText) =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    const data = await res.json();
-    const phones = data.data;
-    // console.log(phones);
-    displayPhones(phones)
-}
+const displayPhones = (phones, isShowAll) => {
+  const phoneContainer = document.getElementById("phone-container");
+  phoneContainer.textContent = "";
 
+  const showAllContainer = document.getElementById("show-all-container");
 
-const displayPhones = phones => {
+  if (phones.length > 12 && !isShowAll) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
 
-    const phoneContainer = document.getElementById('phone-container');
-    phoneContainer.textContent = '';
-    phones.forEach(phone =>{
-        console.log(phone);
-        const phoneCard = document.createElement('div');
-        phoneCard.classList = `card bg-gray-100 p-3 shadow-xl`;
-        phoneCard.innerHTML = `
+  console.log('is nizam', isShowAll);
+  if(!isShowAll){
+    phones = phones.slice(0, 10);
+  }
+
+  phones.forEach((phone) => {
+    // console.log(phone);
+    const phoneCard = document.createElement("div");
+    phoneCard.classList = `card bg-gray-100 p-3 shadow-xl`;
+    phoneCard.innerHTML = `
         <figure><img src="${phone.image}" alt="Shoes" /></figure>
         <div class="card-body">
              <h2 class="card-title">${phone.phone_name}</h2>
@@ -27,17 +40,37 @@ const displayPhones = phones => {
             </div>
          </div>
         `;
-        phoneContainer.appendChild(phoneCard);
-    })
+    phoneContainer.appendChild(phoneCard);
+  });
+
+  toggeleLoadingSpinner(false);
+};
+
+const handelScarch = (isShowAll) => {
+  toggeleLoadingSpinner(true);
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  loadPhone(searchText, isShowAll);
+};
+
+// const handelScarch2 = () => {
+//   toggeleLoadingSpinner(true);
+//   const searchField = document.getElementById("search-field2");
+//   const searchText = searchField.value;
+//   loadPhone(searchText);
+// };
+
+const toggeleLoadingSpinner = (isLoading) => {
+  const loadingSpinner = document.getElementById("loading-spinner");
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
+
+const handelShowAll = () => {
+    handelScarch(true);
 }
-
-const handelScarch = () => {
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    console.log(searchText);
-    loadPhone(searchText);
-}
-
-
 
 // loadPhone();
