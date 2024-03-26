5348cb97bf430928468b994b017931fb4c3fab15
nizamuddin0172 <neom01723441081@gmail.com>
2024-03-27 02:44:03 +0600

    api phone data show

--- script/index.js
+++ script/index.js
@@ -21,7 +21,7 @@ const displayPhones = (phones, isShowAll) => {
     showAllContainer.classList.add("hidden");
   }
 
-  console.log('is nizam', isShowAll);
+  // console.log('is nizam', isShowAll);
   if(!isShowAll){
     phones = phones.slice(0, 10);
   }
@@ -35,8 +35,8 @@ const displayPhones = (phones, isShowAll) => {
         <div class="card-body">
              <h2 class="card-title">${phone.phone_name}</h2>
             <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div class="card-actions justify-end">
-               <button class="btn btn-primary">Buy Now</button>
+              <div class="card-actions justify-center">
+               <button onclick="hendleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
             </div>
          </div>
         `;
@@ -46,19 +46,45 @@ const displayPhones = (phones, isShowAll) => {
   toggeleLoadingSpinner(false);
 };
 
+const hendleShowDetail = async(id) => {
+  // console.log('ok', id);
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json();
+  const phone = data.data;
+
+  showPhoneDetail(phone)
+}
+
+
+
+const showPhoneDetail = (phone) => {
+  console.log(phone);
+  const phoneName = document.getElementById('show-detail-phone-name');
+  phoneName.innerText = phone.name;
+  const showDetailContainer = document.getElementById('show-detail-container');
+  showDetailContainer.innerHTML = `
+  <img class="ml-40 bordrer rounded-2xl" src="${phone.image}" alt="">
+  <p class="font-bold text-xl mt-2 text-white"><span>storage:</span>${phone?.mainFeatures?.storage}</p>
+  <h3 class="font-bold text-xl mt-2 text-white">Display Size : ${phone?.mainFeatures?.displaySize}</h3>
+  <h3 class="font-bold text-xl mt-2 text-white">Chipset : ${phone?.mainFeatures?.chipSet}</h3>
+  <h3 class="font-bold text-xl mt-2 text-white">Memory : ${phone?.mainFeatures?.memory}</h3>
+  <h3 class="font-bold text-xl mt-2 text-white">Slug : ${phone?.slug}</h3>
+  <h3 class="font-bold text-xl mt-2 text-white">Release data : ${phone?.releaseDate}</h3>
+  <h3 class="font-bold text-xl mt-2 text-white">brand : ${phone?.brand}</h3>
+  <h3 class="font-bold text-xl mt-2 text-white">GPS : ${phone?.others?.GPS || 'No GPS'}</h3>
+
+  `;
+  show_details_modal.showModal()
+}
+
+
+
 const handelScarch = (isShowAll) => {
   toggeleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhone(searchText, isShowAll);
 };
-
-// const handelScarch2 = () => {
-//   toggeleLoadingSpinner(true);
-//   const searchField = document.getElementById("search-field2");
-//   const searchText = searchField.value;
-//   loadPhone(searchText);
-// };
 
 const toggeleLoadingSpinner = (isLoading) => {
   const loadingSpinner = document.getElementById("loading-spinner");
